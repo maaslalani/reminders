f48508bdbf897d9fb14e72ee88f895b3e80dc442
Maas Lalani <maaslalani1@gmail.com>
2020-07-05 13:26:29 -0400

    add reminder functionality done

--- src/reminders.rs
+++ src/reminders.rs
@@ -1,8 +1,20 @@
 use crate::util;
+use std::process::Command;
 
 pub fn add() {
     let reminder = util::get_input("Reminder");
     let date = util::parse_date(util::get_input("When"));
+
+    let command = Command::new("osascript")
+        .arg("src/scripts/add.applescript")
+        .arg(&reminder)
+        .arg(&date)
+        .status();
+
+    match command {
+        Ok(_val) => println!("Added \"{}\"", &reminder),
+        Err(_err) => eprintln!("Error: Unable to complete."),
+    };
 }
 
 pub fn complete() {
--- src/scripts/add.applescript
+++ src/scripts/add.applescript
@@ -1,11 +1,9 @@
 on run argv
   tell application "Reminders"
     set reminderName to item 1 of argv
+    set dateTime to item 2 of argv
 
-    set dateTimeString to item 2 of argv
-    set dateTime to date dateTimeString
-
-    make new reminder with properties { name: reminderName, due date: dateTime}
+    make new reminder with properties { name: reminderName, due date: date dateTime}
 
     quit
   end tell
