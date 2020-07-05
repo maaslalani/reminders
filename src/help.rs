6219d63bdfe56dc7a2347a66eb4497f630e774d6
Maas Lalani <maaslalani1@gmail.com>
2020-07-05 15:30:42 -0400

    add usage

--- src/help.rs
+++ src/help.rs
@@ -1,3 +1,16 @@
+const HELP: &str = r#"
+reminders is a command line interface for Reminders.app.
+
+Usage: reminders [ACTION]
+
+    reminders add       - Add a reminder
+    reminders complete  - Mark a reminder as complete
+    reminders help      - Print this usage information
+    reminders list      - List reminders
+
+Running reminders will automatically run reminders add.
+"#;
+
 pub fn usage() {
-    println!("TODO: Help");
+    println!("{}", HELP);
 }
